cd9ced8b872eb9fec8ccea8ceeeaf23cf158c089
Jorge Gabriel <e-jorge2010@hotmail.com>
2020-12-17 19:20:25 -0300

    Back Consuline

---
+++ src/Routes/Private/filiais.js
@@ -0,0 +1,23 @@
+const express = require("express");
+
+const routes = express.Router();
+
+const autorizacaoMid = require("../../middlewares/autorizacao");
+
+const controller = require("../../controllers/filiais");
+
+routes.post("/filial", autorizacaoMid, controller.cadastrar);
+
+routes.get("/filial/:id", controller.buscarPorId);
+
+routes.get("/filiais", controller.listar);
+
+routes.delete("/filial/:id", autorizacaoMid, controller.deletar);
+
+routes.put("/filial/:id", autorizacaoMid, controller.atualizar);
+
+routes.post("/filial/verificar-cnpj", autorizacaoMid, controller.verificarCnpj);
+routes.post("/filial/verificar-ie", autorizacaoMid, controller.verificarIe);
+routes.post("/filial/verificar-nome-fantasia", autorizacaoMid, controller.verificarNomeFantasia);
+
+module.exports = routes;
---
+++ src/Routes/routes.js
@@ -0,0 +1,41 @@
+const express = require("express");
+
+const router = express.Router();
+
+
+// Rotas publicas
+const rotasPublicaSessao = require("./Public/sessao");
+const rotasPublicaPaciente = require("./Public/paciente");
+const rotasPublicaProfissional = require("./Public/profissionalDaSaude");
+
+// Rotas privadas
+const rotasServicos = require("./Private/servicos");
+const rotasFiliais = require("./Private/filiais");
+const rotaAtendimento = require("./Private/atendimento");
+const rotasConsulta = require("./Private/consulta");
+const rotasAvaliacao = require("./Private/avaliacao");
+const rotasNotificacao = require("./Private/notificacao");
+const rotasDadosHome = require("./Private/dadosHome");
+
+router.use(rotasAvaliacao);
+router.use(rotasFiliais);
+router.use(rotasServicos);
+router.use(rotasPublicaSessao);
+
+
+
+
+router.use(rotasPublicaPaciente);
+router.use(rotasPublicaProfissional);
+router.use(rotasConsulta);
+router.use(rotaAtendimento);
+
+router.use(rotasDadosHome);
+
+router.use(rotasAvaliacao);
+
+
+router.use(rotasNotificacao);
+
+
+module.exports = router;
