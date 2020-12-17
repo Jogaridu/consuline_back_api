c476eb6a18ceae5f74105358e265ec413b1269e1
Jorge Gabriel <e-jorge2010@hotmail.com>
2020-12-17 19:38:47 -0300

    Minuscula

--- src/database/seeders/20201214060359-criarTefoneFilial.js
+++ src/database/seeders/20201214060359-criarTefoneFilial.js
@@ -6,31 +6,31 @@ module.exports = {
     await queryInterface.bulkInsert('tblTelefoneFilial', [
       {
         numero: "11912341234",
-        FilialId: 1,
+        filialId: 1,
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
         numero: "11932114567",
-        FilialId: 2,
+        filialId: 2,
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
         numero: "11932113213",
-        FilialId: 3,
+        filialId: 3,
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
         numero: "11932123567",
-        FilialId: 4,
+        filialId: 4,
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
         numero: "11923344112",
-        FilialId: 5,
+        filialId: 5,
         createdAt: new Date(),
         updatedAt: new Date()
       },
