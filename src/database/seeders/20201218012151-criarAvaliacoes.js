bb2c41b64bd6a91c1e31043053f8398e17b2ab9b
Jorge Gabriel <e-jorge2010@hotmail.com>
2020-12-17 22:29:55 -0300

    Letra maiúscula

--- src/database/seeders/20201218012151-criarAvaliacoes.js
+++ src/database/seeders/20201218012151-criarAvaliacoes.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.bulkInsert("tblavaliacao", [
+    await queryInterface.bulkInsert("tblAvaliacao", [
       {
         PacienteId: 1,
         ProfissionalDaSaudeId: 1,
@@ -15,6 +15,6 @@ module.exports = {
   },
 
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.bulkDelete("tblavaliacao", null, {});
+    await queryInterface.bulkDelete("tblAvaliacao", null, {});
   },
 };
