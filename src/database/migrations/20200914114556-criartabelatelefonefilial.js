5e923ae34e3862d4cc28a17a1156199839c954dc
Jorge Gabriel <e-jorge2010@hotmail.com>
2020-12-17 21:45:07 -0300

    Adição maiúscula

--- src/database/migrations/20200914114556-criartabelatelefonefilial.js
+++ src/database/migrations/20200914114556-criartabelatelefonefilial.js
@@ -12,7 +12,7 @@ module.exports = {
         type: Sequelize.STRING,
         allowNull: false,
       },
-      filialId: {
+      FilialId: {
         type: Sequelize.INTEGER,
         allowNull: false,
         references: {
--- src/database/seeders/20201214060359-criarTefoneFilial.js
+++ src/database/seeders/20201214060359-criarTefoneFilial.js
@@ -6,31 +6,31 @@ module.exports = {
     await queryInterface.bulkInsert('tblTelefoneFilial', [
       {
         numero: "11912341234",
-        filialId: 1,
+        FilialId: 1,
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
         numero: "11932114567",
-        filialId: 2,
+        FilialId: 2,
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
         numero: "11932113213",
-        filialId: 3,
+        FilialId: 3,
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
         numero: "11932123567",
-        filialId: 4,
+        FilialId: 4,
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
         numero: "11923344112",
-        filialId: 5,
+        FilialId: 5,
         createdAt: new Date(),
         updatedAt: new Date()
       },
